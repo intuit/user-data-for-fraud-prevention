7c079314ed09587841dbc6e47d1b3888e7b8b7d3
Reuben Evans <reuben_evans@intuit.com>
2020-08-21 17:53:28 +0100

    Updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,9 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    library: 'UserDataForFraudPrevention',
+    libraryTarget: 'umd',
+    globalObject: 'this',
+    umdNamedDefine: true,
   },
 };
