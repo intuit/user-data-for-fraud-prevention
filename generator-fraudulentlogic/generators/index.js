41b1ea1578dd957c82cd1134de071ceb68548f88
Jitin <jitin_maherchandani@intuit.com>
2020-10-05 23:54:30 +0530

    feat: yeoman generator support for new feature

---
+++ generator-fraudulentlogic/generators/index.js
@@ -0,0 +1,21 @@
+var Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+  async prompting() {
+    this.answers = await this.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What's your component called?"
+      }
+    ]);
+  }
+
+  writing() {
+    this.log("component name", this.answers.name);
+    this.fs.copy(
+      this.templatePath('hmrc'),
+      this.destinationPath(`./src/js/${this.answers.name}`)
+    );
+  }
+};
