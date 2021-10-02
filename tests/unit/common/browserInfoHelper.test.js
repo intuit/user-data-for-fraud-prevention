8d24f555e5c1c562b3b7eadb02f55ed0729fd53c
rachana <Mamillapalli.Rachana@iiitb.org>
2021-10-02 17:22:04 +0530

    fix: syntax tests/unit/common/browserInfoHelper.test.js
    Co-authored-by: Susmitha Kodamarthi <f2004156@gmail.com>

--- tests/unit/common/browserInfoHelper.test.js
+++ tests/unit/common/browserInfoHelper.test.js
@@ -266,7 +266,7 @@ describe("BrowserInfoHelper", () => {
   });
 
   it("getUserAgent", async () => {
-    const mockedUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36"
+    const mockedUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36";
     navigatorSpy.mockImplementation(() => ({
       userAgent: mockedUserAgent
     }));
