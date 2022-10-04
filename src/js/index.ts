1c7f20940d2383ad31260545ca2f0a780503b07c
Reuben <reubenae@gmail.com>
2022-10-04 11:20:29 +0100

    DO NOT MERGE
    TEST

--- src/js/index.ts
+++ src/js/index.ts
@@ -10,5 +10,5 @@ export {
   getGovClientTimezoneHeader,
   getGovClientLocalIPsHeader,
   getGovClientWindowSizeHeader,
-  getGovClientScreensHeader
+  getGovClientScreensHeader,
 } from './hmrc/mtdFraudPrevention';
