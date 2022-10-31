303038a07949153ab7d8bbb1653ba5ebc35c7121
Susmitha Kodamarthi <f2004156@gmail.com>
2022-10-31 21:59:04 +0000

    Revert "refactor(typescript): migrate mtdFraudPrevention to TypeScript (#185)"
    This reverts commit 716f0293dacc0b291660df1c69f898e1ed911c36.

--- src/js/common/browserInfoHelper.ts
+++ src/js/common/browserInfoHelper.ts
@@ -24,7 +24,7 @@ export const resetDeviceIpTimeStamp = () => {
  * @returns {Promise} Promise to get users devices IP address
  */
 export const getDeviceLocalIPAsString = (): Promise<
-    typeof deviceIpData
+    typeof deviceIpData | {message: string; error: unknown | undefined}
 > => {
     return new Promise((resolve, reject) => {
         if (deviceIpData.deviceIpString !== '' && deviceIpData.deviceIpTimeStamp !== '') {
